47949c0bc223bc7f060b0467c74b1a1a3e2991a0
Isak <isak.gerre@gmail.com>
2022-03-04 12:42:30 +0100

    sparkline

--- crypto-app/src/components/coinComponent.js
+++ crypto-app/src/components/coinComponent.js
@@ -2,13 +2,15 @@ import React from "react";
 
 function CoinComponent({ coin, index }) {
   console.log(coin);
-
+  let sparklineurl = "http://quickchart.io/chart?c={type:'sparkline',data:{datasets:[{backgroundColor:'rgba(255,0,0,0.2)',borderColor:'red', data:[";
+  sparklineurl = sparklineurl + coin.sparkline + "]}]}}"
   return <tr className="coin_component" key={index}>
         <td className="coin_name"><img src={coin.iconUrl} alt="" className="coin_icon"/>{coin.name}</td>
         <td className="coin_price">{coin.price}</td>
         <td className="coin_change">{coin.change}%</td>
         <td className="coin_24hVolume">{coin["24hVolume"]}</td>
         <td className="coin_rank">{coin.rank}</td>
+    <td className="coin_sparkline"><img src={ sparklineurl} alt="" className="coin_icon"/></td>
   </tr>;
 }
 
